992716dbd8825f1ce693e9660d929a72559b0495
Alex Butler <70113933+MrHacker-X@users.noreply.github.com>
2022-08-30 14:45:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ echo "   \033[1;91m/ / \033[1;97m| | '_ \| | | \ \/ /  \033[1;91m\  / "
 echo "  \033[1;91m/ /__\033[1;97m| | | | | |_| |>  <  \033[1;91m /  \ "
 echo "  \033[1;91m\____/\033[1;97m_|_| |_|\__,_/_/\_\ \033[1;91m/_/\_\\"
 echo "\033[1;91m<\033[1;97m═════\033[1;91m>\033[1;91mCreated By MrHacker-X\033[1;91m<\033[1;97m══════\033[1;91m>"
-exit
 apt-get update -y
 apt-get upgrade -y
 apt-get install python3 -y
-apt-get install wget
+apt-get install wget -y
+apt-get install termux-tools -y
 apt-get install proot -y
 apt-get install proot-distro -y
 mkdir /data/data/com.termux/files/LinuxX
